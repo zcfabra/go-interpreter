7c1e035b090f000073e3a53a62ea85440ed052be
zcfabra <christianwgnam@gmail.com>
2024-03-16 17:37:29 -0400

    repl

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"lang/repl"
+	"os"
+	"os/user"
+)
+
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("[%s] START REPL SESSION\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 
 }
